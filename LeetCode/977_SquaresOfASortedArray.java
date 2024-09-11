5c4132ab087e072f4ef9bab7f2ba6375946f62a6
Robin Rodrigues <robinrodrigues6499@gmail.com>
2024-09-11 11:49:47 +0530

    Solved problem no 977

---
+++ LeetCode/977_SquaresOfASortedArray.java
@@ -0,0 +1,47 @@
+// 1. Pattern: Two Pointers [Squaring a Sorted Array (easy)]
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+
+        int[] sorted = new int[nums.length];
+        int indexOfZero = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= 0) {
+                indexOfZero = i;
+                break;
+            }
+        }
+
+        int i1 = (indexOfZero - 1);
+        int i2 = indexOfZero;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (i1 == -1 || i2 == nums.length) {
+
+                if (i1 >= 0) {
+                    sorted[i] = nums[i1] * nums[i1];
+                    i1--;
+                } else {
+                    sorted[i] = nums[i2] * nums[i2];
+                    i2++;
+                }
+
+            } else {
+
+                if ((nums[i1] * nums[i1]) < (nums[i2] * nums[i2])) {
+                    sorted[i] = nums[i1] * nums[i1];
+                    i1--;
+                } else {
+                    sorted[i] = nums[i2] * nums[i2];
+                    i2++;
+                }
+
+            }
+
+        }
+
+        return sorted;
+
+    }
+}
