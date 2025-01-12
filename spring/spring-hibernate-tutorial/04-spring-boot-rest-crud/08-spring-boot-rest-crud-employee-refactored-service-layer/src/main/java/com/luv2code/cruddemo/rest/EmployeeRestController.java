e18f3a556109338a7860e39860ffda40ff2256d1
Robin Rodrigues <robinrodrigues6499@gmail.com>
2025-01-12 16:59:26 +0530

    Revision of Spring Hibernate Tutorial - Section 4 done [INTERVIEW PREPS]

---
+++ spring/spring-hibernate-tutorial/04-spring-boot-rest-crud/08-spring-boot-rest-crud-employee-refactored-service-layer/src/main/java/com/luv2code/cruddemo/rest/EmployeeRestController.java
@@ -0,0 +1,29 @@
+package com.luv2code.cruddemo.rest;
+
+import com.luv2code.cruddemo.entity.Employee;
+import com.luv2code.cruddemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private EmployeeService employeeService;
+
+    // quick and dirty: inject employee dao
+    @Autowired
+    public EmployeeRestController(EmployeeService theEmployeeService) {
+        employeeService = theEmployeeService;
+    }
+
+    // expose "/employees" and return a list of employees
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return employeeService.findAll();
+    }
+}
